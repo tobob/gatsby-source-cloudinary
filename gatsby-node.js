b8d8706568b83b3ce9b9e99ccfdb9bbfcf5f3408
Wojciech Bozek <bozek.wojciech@gmail.com>
2020-03-04 20:45:44 +0100

    Increase limit to 1000

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const {newCloudinary, getResourceOptions} = require('./utils');
+const { newCloudinary, getResourceOptions } = require('./utils');
 const type = `CloudinaryMedia`;
 
 const getNodeData = (gatsby, media) => {
@@ -14,41 +14,76 @@ const getNodeData = (gatsby, media) => {
   };
 };
 
-const addTransformations = (resource, transformation, secure)=>{
+const addTransformations = (resource, transformation, secure) => {
   const splitURL = secure ? resource.secure_url.split('/') : resource.url.split('/');
-  splitURL.splice( 6, 0, transformation);
+  splitURL.splice(6, 0, transformation);
 
   const transformedURL = splitURL.join('/');
   return transformedURL;
-      
 }
 
 const createCloudinaryNodes = (gatsby, cloudinary, options) => {
-  return cloudinary.api.resources(options, (error, result) => {
-    const hasResources = (result && result.resources && result.resources.length);
+  return new Promise((resolve, reject) => {
+    cloudinary.api.resources(options, (error, result) => {
+      const hasResources = (result && result.resources && result.resources.length);
 
-    if (error) {
-      console.error(error);
-      return;
-    }
+      if (error) {
+        console.error(error);
+        reject(error);
+        return;
+      }
 
-    if (!hasResources) {
-      console.warn('\n ~Yikes! No nodes created because no Cloudinary resources found. Try a different query?');
-      return;
-    }
+      if (!hasResources) {
+        console.warn('\n ~Yikes! No nodes created because no Cloudinary resources found. Try a different query?');
+        resolve();
+        return;
+      }
 
-    result.resources.forEach(resource => {
-      const transformations = "q_auto,f_auto" // Default CL transformations, todo: fetch base transformations from config maybe.  
-      
-      resource.url = addTransformations(resource, transformations);
-      resource.secure_url = addTransformations(resource, transformations, true);
+      let nextCursor = result.next_cursor;
 
-      const nodeData = getNodeData(gatsby, resource);
-      gatsby.actions.createNode(nodeData);
+      result.resources.forEach(resource => {
+        const transformations = "q_auto,f_auto" // Default CL transformations, todo: fetch base transformations from config maybe.
+
+        resource.url = addTransformations(resource, transformations);
+        resource.secure_url = addTransformations(resource, transformations, true);
+
+        const nodeData = getNodeData(gatsby, resource);
+        gatsby.actions.createNode(nodeData);
+      });
+      if (!nextCursor) {
+        resolve()
+      }
+
+      cloudinary.api.resources({ ...options, next_cursor: result.next_cursor }, (error, result) => {
+        const hasResources = (result && result.resources && result.resources.length);
+        console.info('If this is equal 500 then you need to fix cloudinary again', hasResources);
+
+        if (error) {
+          console.error(error);
+          reject(error);
+          return;
+        }
+
+        if (!hasResources) {
+          console.warn('\n ~Yikes! No nodes created because no Cloudinary resources found. Try a different query?');
+          resolve();
+          return;
+        }
+
+        result.resources.forEach(resource => {
+          const transformations = "q_auto,f_auto" // Default CL transformations, todo: fetch base transformations from config maybe.
+
+          resource.url = addTransformations(resource, transformations);
+          resource.secure_url = addTransformations(resource, transformations, true);
+
+          const nodeData = getNodeData(gatsby, resource);
+          gatsby.actions.createNode(nodeData);
+        });
+
+        resolve();
+      });
     });
-
-    console.info(`Added ${hasResources} CloudinaryMedia ${hasResources > 1 ? 'nodes' : 'node'}`);
-  });
+  })
 };
 
 exports.sourceNodes = (gatsby, options) => {
